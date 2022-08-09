10d0109981ad4f2d2683fecbfc6871aa2d469519
Deise Costa <deisecj@gmail.com>
2022-08-08 21:14:15 -0400

    update browser margins

--- src/App.js
+++ src/App.js
@@ -5,12 +5,16 @@ import {
   Route
 } from "react-router-dom";
 import Home from "./pages/Home";
+import Company from "./pages/Company";
+import NewCompany from "./pages/NewCompany";
 
 export default function App() {
   return (
     <Router>
       <Routes> 
         <Route path="/" element={<Home/>} />
+        <Route path="/companies/:id" element={<Company/>} />
+        <Route path="/companies/" element={<NewCompany/>} />
       </Routes>
     </Router>
   );
