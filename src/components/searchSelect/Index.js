7dd6a405ffa861417a5b5c700da633f853e344f5
Deise Costa <deisecj@gmail.com>
2022-08-09 11:00:42 -0400

    fixed search company

--- src/components/searchSelect/Index.js
+++ src/components/searchSelect/Index.js
@@ -4,7 +4,7 @@ import { StarIcon } from '@heroicons/react/outline';
 import { useNavigate } from "react-router-dom";
 import { SearchIcon } from '@heroicons/react/solid';
 
-const company = [
+const companies = [
   { id: 1, name: 'IBM North America', url: '#' },
   { id: 2, name: 'IBM', url: '#' },
   { id: 3, name: 'Amazon US', url: '#' },
@@ -18,15 +18,17 @@ const SearchSelect = () => {
   const [query, setQuery] = useState('');
   const navigate = useNavigate();
 
-  const filteredCompany =
+  const filteredCompanies =
     query === ''
       ? []
-      : company.filter((company) => {
+      : companies.filter((company) => {
         return company.name.toLowerCase().includes(query.toLowerCase())
       })
-  
-  const handleSelectCompany = () => {
-    navigate('/companies/1');
+
+  const handleSelectCompany = (company) => {
+    if (company) {
+      navigate(`/companies/${company.id}`);
+    }
   }
 
   const handleRateMyEmployer = () => {
@@ -34,16 +36,11 @@ const SearchSelect = () => {
   }
 
   const onChangeInput = (event) => {
-    console.log("passou aqui", event.target.value)
-     setQuery(event.target.value);
-  }
-
-  const onLeaveInput = () => {
-    setQuery('');
+    setQuery(event.target.value);
   }
 
   return (
-    <Combobox onChange={(company) => (window.location = company.url)}>
+    <Combobox onChange={handleSelectCompany}>
       <div className="relative">
         <SearchIcon
           className="pointer-events-none absolute top-3.5 left-4 h-5 w-5 text-gray-400"
@@ -53,15 +50,13 @@ const SearchSelect = () => {
           className="h-12 w-full focus:ring-indigo-500 focus:border-indigo-500 rounded-md border-0 bg-gray-100 px-4 py-2.5 pl-11 text-gray-900 placeholder-gray-500 sm:text-sm"
           placeholder="Search companies"
           onChange={onChangeInput}
-          onBlur={onLeaveInput}
         />
       </div>
-      {filteredCompany.length > 0 && (
+      {filteredCompanies.length > 0 && (
         <Combobox.Options
-          
           className="bg-white rounded-md -mb-2 max-h-72 scroll-py-2 overflow-y-auto py-2 text-sm text-gray-800"
         >
-          {filteredCompany.map((company) => (
+          {filteredCompanies.map((company) => (
             <Combobox.Option
               key={company.id}
               value={company}
@@ -70,29 +65,33 @@ const SearchSelect = () => {
                   'cursor-default select-none rounded-md px-4 py-2',
                   active && 'bg-neutral-100 text-neutral-900'
                 )
-              }
-              onClick={handleSelectCompany}
-            >
+              }>
               {company.name}
             </Combobox.Option>
           ))}
         </Combobox.Options>
       )}
 
-      {query !== '' && filteredCompany.length === 0 && (
-        <div className="bg-white py-4 px-4 sm:px-14 grid justify-items-center">
-          <p className="mt-5 text-sm text-gray-900">Oops... we didn't find any companies that match this search.</p>
-          <p className="mt-5 text-sm text-gray-400">Try refining your search or search for another company.</p>
-          <img src="./images/company-not-found.svg"  alt="" className="m-10"/>
-          <button
-            onClick={handleRateMyEmployer}
-            type="button"
-            className="mt-5 inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-          >
-              <StarIcon className="-ml-1 mr-2 h-5 w-5 bg-200" aria-hidden="true" />
-              Rate my employer
-            </button>
-        </div>
+      {query != '' && filteredCompanies.length === 0 && (
+        <Combobox.Options
+          className="bg-white rounded-md -mb-2 scroll-py-2 overflow-y-auto py-2 text-sm text-gray-800"
+        >
+          <Combobox.Option value={null}>
+            <div className="bg-white py-4 px-4 sm:px-14 grid justify-items-center">
+              <p className="mt-5 text-sm text-gray-900">Oops... we didn't find any companies that match this search.</p>
+              <p className="mt-5 text-sm text-gray-400">Try refining your search or search for another company.</p>
+              <img src="./images/company-not-found.svg" alt="" className="m-10" />
+              <button
+                onClick={handleRateMyEmployer}
+                type="button"
+                className="mt-5 inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              >
+                <StarIcon className="-ml-1 mr-2 h-5 w-5 bg-200" aria-hidden="true" />
+                Rate my employer
+              </button>
+            </div>
+          </Combobox.Option>
+        </Combobox.Options>
       )}
     </Combobox>
   )
