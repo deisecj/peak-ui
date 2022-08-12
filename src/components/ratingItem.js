a50c890b6fe3a34af3d5d5b1f3e4ccf03093a734
Deise Costa <deisecj@gmail.com>
2022-08-11 20:40:40 -0400

    fix margim

--- src/components/ratingItem.js
+++ src/components/ratingItem.js
@@ -2,7 +2,7 @@ import Rating from "./rating";
 
 const RatingItem = ({ itemName, className }) => {
   return (
-    <div className={"grid grid-cols-2 " + className}>
+    <div className={"grid grid-cols-2 mb-7 " + className}>
       <p className="text-lg font-semibold text-brand-300">{itemName}</p>
       <div className="mt-2 grid justify-end">
         <Rating />
