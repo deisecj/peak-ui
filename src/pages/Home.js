b071d65a1369ec1d868993865b05492a936417a7
Deise Costa <deisecj@gmail.com>
2022-08-29 14:58:24 -0400

    creating new layout for the browser version

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -27,16 +27,16 @@ const Home = () => {
   }
 
   return (
-    <div id="home" className="mx-auto max-w-screen-2xl h-full">
-      <div className="hero-section">
-      <Disclosure as="header" className="bg-gray-800">
+    <div id="home" className="">
+       <div className="hero-bg-header">
+      <Disclosure as="header" className="hero-section">
       {({ open }) => (
         <>
         <div className="hero-header-navbar">
           <div className="hero-header">
             <img
               className="hero-logo"
-              src="./images/happy-jobs-logo.svg"
+              src="/images/peak-logo.svg"
               alt=""
             />
 
@@ -67,50 +67,61 @@ const Home = () => {
         </>
       )}
       </Disclosure>
+      </div>
+      <div className="header-bg">
         <div className="hero-content">
-          <h1 className="title-text">REAL. HONEST. VERIFIED.</h1>
-          <img src="./images/hero-image.svg" className="hero-image" />
+          <div className="hero-title">
+              <h1 className="">REAL. HONEST. VERIFIED.</h1>
+          </div>
         </div>
-      </div>
-      <div className="explore-section">
-        <div className="search-section">
-          <div className="">
-            <p className="sub-title-text">Explore the companies that interest you.</p>
-          </div>
-          <div className="search-item">
-            <SearchSelect 
-              createEnable={false}
-              onSelect={handleOnSelectCompany}
-              className="h-12 w-full focus:ring-indigo-500 focus:border-indigo-500 rounded-md border-0 bg-gray-100 px-4 py-2.5 pl-11 text-gray-900 placeholder-gray-500 sm:text-sm"
-              classNameSearchIcon="search-icon"
-              placeHolderText="Search companies"
-             />
-          </div>
-          <div className="information-section flex justify-between gap-x-8">
-            <div className="information-section-item">
-              <p>Get in-depth information about company culture shared by verified employees.</p>
+        <div className='hero-content'>
+          <div className="grid grid-cols-2 gap-20 justify-between mx-64">
+            <div className="explore-section">
+              <p className="mb-40 text-neutral-900 text-2xl leading-9 font-normal">Understanding workplace culture doesn’t have to be so hard</p>
+              <div className="">
+                <div className="">
+                  <p className="search-label">Find culture reviews from actual employees</p>
+                </div>
+                <div className="search-item">
+                  <SearchSelect
+                    createEnable={false}
+                    onSelect={handleOnSelectCompany}
+                    className="h-12 w-full focus:ring-indigo-500 focus:border-indigo-500 rounded-md border bg-white px-4 py-2.5 pl-11 text-gray-900 placeholder-neutral-400 placeholder-text-base sm:text-sm"
+                    classNameSearchIcon="search-icon"
+                    placeHolderText="Search companies"
+                  />
+                </div>
+              </div>
             </div>
-            <div className="information-section-item">
-              <p>Discover company ratings to ensure you will find your happy place in the work place.</p>
+            <div className='mt-6'>
+              <img src="./images/hero-image.svg" className="hero-image" />
             </div>
           </div>
         </div>
       </div>
-      <div className="contribute-section">
-        <div className="contribute-content">
-          <img src="./images/rate-employer.svg" alt="" className="contribute-image" />
-          <div className="rate-section lg:mt-16">
-            <p className="mb-10">Contribute to a growing community and help others reach new heights in their career.</p>
-            <button onClick={handleOpenModal} type="button" className="mt-5 inline-flex justify-self-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-              <StarIcon className="-ml-1 mr-2 h-5 w-5 bg-200" aria-hidden="true" />
-              Rate my employer
-            </button>
-            {isOpen && <ModalRating openModal={isOpen} closeModal={() => setIsOpen(false)}/>}
+      <div className="hero-content">
+        <div className="contribute-section">
+          <div className="contribute-content">
+            <img src="./images/rate-employer.svg" alt="" className="contribute-image" />
+            <div className="rate-section lg:mt-16">
+              <p className="text-neutral-900 text-2xl leading-10">Contribute to a growing community</p>
+              <button onClick={handleOpenModal} type="button" className="mt-5 inline-flex justify-self-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                <StarIcon className="-ml-1 mr-2 h-5 w-5 bg-200" aria-hidden="true" />
+                Rate my employer
+              </button>
+              {isOpen && <ModalRating openModal={isOpen} closeModal={() => setIsOpen(false)} />}
+              <div className="information-section-item mt-14 mb-10">
+                <p>Your company rating makes an impact with a community of job hunters just like you.</p>
+              </div>
+              <div className="information-section-item">
+                <p>Share relatable experiences without including your location or position, unless you want to.</p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-      <Footer menuClassName="menu-section" footerClassName="footer-section"/>
-    </div>
+        <Footer menuClassName="menu-section" footerClassName="footer-section"/>
+  </div> 
   );
 }
 
